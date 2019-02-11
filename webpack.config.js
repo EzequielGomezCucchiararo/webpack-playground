143c4e3bf72fa0fcf9509e4e2d7fca5f2603c034
Ezequiel Gomez <ezequiel.cucchiararo@gmail.com>
2019-02-11 23:28:29 +0100

    HMR implemented

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
@@ -6,13 +7,20 @@ module.exports = {
 	mode: 'development',
 	entry: {
 		app: './src/index.js',
-		print: './src/print.js',
+	},
+	module: {
+		rules: [{
+			test: /\.css$/,
+			use: ['style-loader', 'css-loader']
+		}]
 	},
 	devtool: 'inline-source-map',
 	devServer: {
-		contentBase: './dist'
+		contentBase: './dist',
+		hot: true
 	},
 	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
 		new CleanWebpackPlugin(['dist']),
 		new HtmlWebpackPlugin({
 			title: 'Output Management'
