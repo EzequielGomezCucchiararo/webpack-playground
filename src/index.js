6d0153d0a630b36803bc7346863fb8439134f041
Ezequiel Gomez <ezequiel.cucchiararo@gmail.com>
2019-02-11 22:11:57 +0100

    Loading Images

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import _ from 'lodash';
 import './style.css';
+import Plato from './Plato.jpg';
 
 function component() {
-  let element = document.createElement('div');
+	const element = document.createElement('div');
+	const platoPng = new Image();
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+	element.classList.add('hello');
 
-  return element;
+	platoPng.src = Plato;
+
+	element.appendChild(platoPng);
+
+	return element;
 }
 
 document.body.appendChild(component());
